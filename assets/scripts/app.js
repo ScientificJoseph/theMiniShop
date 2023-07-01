6a01b8ef8227c306b2818c58479f9379aa0182b9
TekniqueJoseph <tekniquejoseph@gmail.com>
2023-07-01 11:11:29 -0400

    added product list items

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,17 +1,41 @@
 const productList = {
-    products: [
-    {
-        title: 'A Pillow',
-        imageUrl: 'http://tiny.cc/en48vz',
-        price: 19.99,
-        description: 'The Pillow Of Manifestation'
-    },
-    {
-        title: 'A Rug',
-        imageUrl: 'http://tiny.cc/co48vz',
-        price: 89.99,
-        description: 'Like Walking On A Cloud'
+        products: [
+        {
+            title: 'A Pillow',
+            imageUrl: 'http://tiny.cc/en48vz',
+            price: 19.99,
+            description: 'The Pillow Of Manifestation'
+        },
+        {
+            title: 'A Rug',
+            imageUrl: 'http://tiny.cc/co48vz',
+            price: 89.99,
+            description: 'Like Walking On A Cloud'
+        }
+    ],
+    render() {
+        const renderHook = document.getElementById('app');
+        const prodList = document.createElement('ul');
+        prodList.className = 'product-list';
+        for (const prod of this.products){
+            const prodEl = document.createElement('li');
+            prodEl.className = 'product-item';
+            prodEl.innerHTML = `
+                <div>
+                    <img src='${prod.imageUrl}' alt=${prod.title}>
+                    <div class ='product-item__content'>
+                        <h2>${prod.title}</h2>
+                        <h3>\$${prod.price}</h3>
+                        <p>${prod.description}</p>
+                    </div>
+                </div>
+                
+            `;
+            prodList.append(prodEl);
+        }
+        renderHook.append(prodList);
     }
-]
-
 };
+
+productList.render()
+
