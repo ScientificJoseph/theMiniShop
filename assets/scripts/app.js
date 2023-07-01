13a21558dde349a0e03f713a6881cfe2db59927c
TekniqueJoseph <tekniquejoseph@gmail.com>
2023-07-01 10:32:44 -0400

    first commit

---
+++ assets/scripts/app.js
@@ -0,0 +1,17 @@
+const productList = {
+    products: [
+    {
+        title: 'A Pillow',
+        imageUrl: 'http://tiny.cc/en48vz',
+        price: 19.99,
+        description: 'The Pillow Of Manifestation'
+    },
+    {
+        title: 'A Rug',
+        imageUrl: 'http://tiny.cc/co48vz',
+        price: 89.99,
+        description: 'Like Walking On A Cloud'
+    }
+]
+
+};
